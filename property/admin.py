e44f321353de3e544123b6ee9d067bb93328313b
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-07 12:06:16 +0300

    Customize Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,7 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
+    readonly_fields = ['created_at']
 
 
 admin.site.register(Flat, FlatAdmin)
