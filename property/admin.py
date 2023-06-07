684898faff21f20d183f090b017cf4ed22aa2316
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-07 13:55:17 +0300

    Setup filtering in admin UI

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
 
 admin.site.register(Flat, FlatAdmin)
