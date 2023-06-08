772fb7a608d3b0437755fc3a70991384fa3e79e0
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-08 12:34:10 +0300

    The apartment fields are now connected with interlocutors

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, null=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField(null=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
@@ -57,10 +54,11 @@ class Flat(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owned_apartments = models.ManyToManyField(Flat, related_name='owned_apartments', verbose_name='Квартиры в собственности')
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    owned_apartments = models.ManyToManyField(Flat, related_name='owned_apartments',
+                                              verbose_name='Квартиры в собственности')
 
     def __str__(self):
         return self.owner
