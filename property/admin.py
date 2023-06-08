b0fa19160dada1b236e54f9245b20a194485375b
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-08 13:20:21 +0300

    Setup admininline in admin UI

--- property/admin.py
+++ property/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
-
 from .models import Flat, Complaint, Owner
 
 
+class AdminInline(admin.TabularInline):
+    model = Owner.owned_apartments.through
+    raw_id_fields = ['owner', 'flat']
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address', 'owned_apartments__owner']
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+    inlines = [AdminInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
